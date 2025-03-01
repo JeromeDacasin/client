00007dedfdd4f376d1828c064d987707961ded82
acxe21 <jeromedacasin21@gmail.com>
2025-03-01 17:06:04 +0800

    add additional information in landing page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,12 +1,26 @@
-// import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import style from './../styles/notfound.module.css';
 
 
 const NotFound = () => {
     return (
-        <div id={style.container}>
-            {/* <Link to="/home">Home from Link</Link> */}
-            <h1>404 - Not Found</h1>
+        <div id={style.container} style={{ textAlign: "center", padding: "50px" }}>
+            <h1>🚧 Page Under Construction 🚧</h1>
+            <p>Oops! The page you’re looking for is not available or still under development.</p>
+            <Link to="/login">
+                <button style={{
+                    padding: "10px 20px",
+                    fontSize: "16px",
+                    cursor: "pointer",
+                    backgroundColor: "#007bff",
+                    color: "#fff",
+                    border: "none",
+                    borderRadius: "5px",
+                    marginTop: "20px"
+                }}>
+                    Go to Login
+                </button>
+            </Link>
         </div>
     )
 }
